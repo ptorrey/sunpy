6b7bba8881b379963f346d037b788137b8b699a6
Paul Torrey <ptorrey@cfa.harvard.edu>
2015-01-24 16:54:24 -0500

    removed auto background download from example scripts.

---
+++ examples/download_backgrounds.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+""" Download the background images used for creating the mock GZ images.
+    Author: P. Torrey (ptorrey@mit.edu) 1/24/15
+"""
+import sunpy.sunpy__synthetic_image
+
+sunpy__synthetic_image.download_backgrounds()
+
